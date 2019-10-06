2a96b04c7d8f085ac8b4a3a241c030d4fa162ad5
Antoine <antozzzdev@gmail.com>
2019-10-06 20:59:26 +0200

    Initialisation de VertX

---
+++ src/main/java/com/tekken/Start.java
@@ -0,0 +1,27 @@
+package com.tekken;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+public class Start extends AbstractVerticle {
+
+    public Start() {
+
+    }
+
+    public void start() {
+
+        final Router router = Router.router(vertx);
+
+        router.route().handler(BodyHandler.create());
+        //router.post("/auth/login").handler(new handlePostLogin(replication, cache));
+        //router.post("/auth/token").handler(new handlePostToken(cache));
+
+        router.route().handler(StaticHandler.create("webroot"));
+        vertx.createHttpServer().requestHandler(router).listen(80);
+
+    }
+
+}
--- src/main/java/com/tekken/Tekken.java
+++ src/main/java/com/tekken/Tekken.java
@@ -1,13 +1,32 @@
 package com.tekken;
 
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+
+import java.util.function.Consumer;
+
 public class Tekken {
 
     public Tekken() {
-        
+/*
+        cache = new Cache(redisHost, redisPort, poolInteger);
+        replication = new Replication(poolInteger);
+*/
+        Consumer<Vertx> runner = vertx -> {
+            //for (int i=0;i<poolInteger; i++)
+                vertx.deployVerticle(new Start(), new DeploymentOptions());
+        };
+
+        Vertx vertx = Vertx.vertx(new VertxOptions());
+        runner.accept(vertx);
+
     }
 
-    public static void main(){
+    public static void main(String[] args){
+        System.out.println(" * Tekken (By AntoZzz)");
         new Tekken();
     }
+
 
 }
